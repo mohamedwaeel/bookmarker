5f80f73d67c541452fe42b5029292ad2039cb237
waelmw <Mohamed505.mw@gmail.com>
2023-12-21 11:06:20 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,112 @@
+"use strict"
+
+let siteName = document.querySelector("#sitename");
+let siteUrl = document.querySelector("#siteurl");
+let sitesList = [];
+let sbmtBtn = document.querySelector("#submitButton")
+sbmtBtn.addEventListener('click', () => {
+    addSites();
+    displaySites(sitesList);
+
+})
+
+
+if (localStorage.getItem("allSites")) {
+    sitesList = JSON.parse(localStorage.getItem("allSites"));
+    displaySites(sitesList);
+}
+
+function displaySites(list) {
+    let blackBox = " ";
+    for (let i = 0; i < list.length; i++) {
+        blackBox += `<tr>
+  <th scope="row">${i + 1}</th>
+  <td class="text-capitalize">${list[i].name}</td>
+  <td><button id="visitbtn" class="btn" onclick="visitSite('${list[i].url}')"><i class="fa-solid fa-eye"></i> Visit</button></td>
+  <td><button class="btn btn-danger" onclick="deleteSite(${i})"><i class="fa-solid fa-trash-can"></i> Delete</button>
+  </td>
+</tr>`
+    }
+    document.querySelector("tbody").innerHTML = blackBox;
+}
+
+function deleteSite(index) {
+    sitesList.splice(index, 1);
+    localStorage.setItem("allSites", JSON.stringify(sitesList));
+    displaySites(sitesList);
+}
+
+function addSites() {
+    if (validSiteUrl() && validSiteName()) {
+        let sites = {
+            name: siteName.value,
+            url: siteUrl.value
+        };
+        sitesList.push(sites);
+        localStorage.setItem("allSites", JSON.stringify(sitesList));
+        clearForm();
+    }
+
+}
+function clearForm() {
+    siteName.value = "";
+    siteUrl.value = "";
+    document.getElementById("validName").classList.add("d-none");
+    document.getElementById("validUrl").classList.add("d-none");
+
+
+}
+function visitSite(url) {
+    window.open(url, '_blank');
+}
+
+function validSiteName() {
+    let regex =/^(?!\s+$)[a-zA-Z\s]+$/;
+    let isValid = regex.test(siteName.value);
+    if (isValid) {
+        document.getElementById("sitename").classList.add("is-valid");
+        document.getElementById("sitename").classList.remove("is-invalid");
+        document.getElementById("validName").classList.remove("d-none");
+        document.getElementById("inValidName").classList.add("d-none");
+
+
+    }
+    else {
+        document.getElementById("sitename").classList.remove("is-valid");
+        document.getElementById("sitename").classList.add("is-invalid");
+        document.getElementById("inValidName").classList.remove("d-none");
+        document.getElementById("validName").classList.add("d-none");
+
+
+    }
+    return isValid;
+}
+
+function validSiteUrl() {
+ 
+    let regex = /^(https:\/\/|http:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/i;
+    let isValid = regex.test(siteUrl.value);
+
+    if (isValid) {
+        document.getElementById("siteurl").classList.add("is-valid");
+        document.getElementById("siteurl").classList.remove("is-invalid");
+        document.getElementById("validUrl").classList.remove("d-none");
+        document.getElementById("inValidUrl").classList.add("d-none");
+
+
+    }
+    else {
+        document.getElementById("siteurl").classList.remove("is-valid");
+        document.getElementById("siteurl").classList.add("is-invalid");
+        document.getElementById("inValidUrl").classList.remove("d-none");
+        document.getElementById("validUrl").classList.add("d-none");
+
+
+    }
+    return isValid;
+
+}
+let siteNameValidation=document.querySelector("#sitename");
+siteNameValidation.addEventListener('input',()=>{return validSiteName()})
+let siteUrlValidation=document.querySelector("#siteurl");
+siteUrlValidation.addEventListener('input',()=>{return validSiteUrl()})
